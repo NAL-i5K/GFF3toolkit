c2d1892da2e1729b93bf03726b4cab9ad99530d7
dytk2134 <dytk2134@gmail.com>
2017-08-11 05:56:16 +0800

    gff3_sort.py add try/except

--- bin/gff3_sort.py
+++ bin/gff3_sort.py
@@ -121,7 +121,14 @@ def main(gff, output=None, logger=None):
     logger.info('Sorting and printing out...')
     
     # Visit the GFF3 object through root-level features (eg. gene, pseudogene, and etc.)
-    roots = [line for line in gff3.lines if line['line_type'] == 'feature' and not line['attributes'].has_key('Parent')]
+    roots =[]
+    for line in gff3.lines:
+       try:
+           if line['line_type'] == 'feature' and not line['attributes'].has_key('Parent'):
+               roots.append(line)
+       except:
+           logger.warning('[Missing Attributes] Program failed.\n\t\t- Line {0:s}: {1:s}'.format(str(line['line_index']+1), line['line_raw'])) 
+    #roots = [line for line in gff3.lines if line['line_type'] == 'feature' and not line['attributes'].has_key('Parent')]
 
     # Sort the root-level features based on the order of the genomic sequences
     roots_sorted = PositionSort(roots)
