a069040eab34207323537c24520dfe07ee8c53bf
dytk2134 <dytk2134@gmail.com>
2017-12-05 09:45:26 -0500

    fix fixing function

--- bin/gff3_fix.py
+++ bin/gff3_fix.py
@@ -11,14 +11,16 @@ import re
 import logging
 # try to import from project first
 from os.path import dirname
+import version
 if dirname(__file__) == '':
     lib_path = '../lib'
 else:
     lib_path = dirname(__file__) + '/../lib'
 sys.path.insert(1, lib_path)
+sys.path =  filter (lambda a: not a.endswith('/bin'), sys.path)
 from gff3_modified import Gff3
 import gff3_fix
-import version
+
 __version__ = version.__version__
    
 
@@ -52,12 +54,12 @@ if __name__ == '__main__':
 
     parser.add_argument('-qc_r', '--qc_report', type=str, help='Error report from gff3_QC.py')
     parser.add_argument('-g', '--gff', type=str, help='Genome annotation file, gff3 format')
-    parser.add_argument('-r', '--report', type=str, help='output report file name')
+    #parser.add_argument('-r', '--report', type=str, help='output report file name')
     parser.add_argument('-og', '--output_gff', type=str, help='output gff3 file name')
     parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
 
     args = parser.parse_args()
-    logger_stderr.infor('Reading QC report file: (%s)...\n', args.qc_report)
+    logger_stderr.info('Reading QC report file: (%s)...\n', args.qc_report)
     #error_dict example: {'Emr0001': [[15,16],[13]],'Esf0005': [[17]]}
     error_dict = {}
     #line_num_dict example: {3: ['Emr0001','Esf0003'], 15: ['Emr0026']}
@@ -68,29 +70,32 @@ if __name__ == '__main__':
             next(qcr)
             for line in qcr:
                 line = line.strip()
-                lines = line.split("\t")
-                line_num_list = map(int,re.findall(r'\d+',lines[0]))
-                if lines[1] not in error_dict:
-                    error_dict[lines[1]] = [line_num_list]
-                else:
-                    error_dict[lines[1]].append(line_num_list)
-                for line_num in line_num_list:
-                    if line_num not in line_num_dict:
-                        line_num_dict[line_num] = {lines[1]: [line[2]]}
-                    else:
-                        if line[1] not in line_num_dict[line_num]:
-                            line_num_dict[line_num][line[1]] = [line[2]]
+                if line:
+                    try:
+                        lines = line.split("\t")
+                        line_num_list = map(int,re.findall(r'\d+',lines[0]))
+                        if lines[1] not in error_dict:
+                            error_dict[lines[1]] = [line_num_list]
                         else:
-                            line_num_dict[line_num][line[1]].append(line[2])
+                            error_dict[lines[1]].append(line_num_list)
+                        for line_num in line_num_list:
+                            if line_num not in line_num_dict:
+                                line_num_dict[line_num] = {lines[1]: lines[2]}
+                            else:
+                                line_num_dict[line_num][lines[1]] = lines[2]
+                    except:
+                        logger_stderr.warning('Failed to recognize - %s', line)
 
     except:
         logger_stderr.error('Failed to read QC report file!')
-    logger_stderr.infor('Reading GFF3 file: (%s)...\n', args.gff)
+    logger_stderr.info('Reading GFF3 file: (%s)...\n', args.gff)
     try:
         gff3 = Gff3(gff_file=args.gff, logger=logger_null)
     except:
         logger_stderr.error('Failed to read GFF3 file!')
-    gff3_fix.fix.main(gff3=gff3, output_gff=args.out_gff, report=args.report, error_dict=error_dict, line_num_dict=line_num_dict, logger=logger_null)
+
+    gff3_fix.fix.main(gff3=gff3, output_gff=args.output_gff, error_dict=error_dict, line_num_dict=line_num_dict, logger=logger_null)
+
 
 
                        
