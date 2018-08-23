9d1c68ad590d22bf39b7b1a07c81f6be6125edc9
Yi Hsiao <hsiaoyi0504@gmail.com>
2018-08-23 12:58:21 -0400

    import explicitly

--- gff3tool/lib/intra_model/__init__.py
+++ gff3tool/lib/intra_model/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Extract sequences from specific regions of genome based on gff file."""
 from __future__ import absolute_import
-from .intra_model import *
+from .intra_model import main
 
 __homepage__ = 'https://github.com/NAL-i5K/GFF3toolkit'
+__all__ = ['main']
