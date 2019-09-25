98bf0560fe40eae5e0f1caa63bc00edf8ef901a1
ShangYuChiang <shelly200318@hotmail.com.tw>
2019-09-25 11:05:47 -0400

    Update setup

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from os import path, remove, mkdir
 import shutil
 import tarfile
 import sys
-from urllib.request import urlretrieve
+import urllib.request
 import platform
 from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 
@@ -38,15 +38,15 @@ class CustomBuildCommand(build):
         mkdir(blast_path)
 
         if platform_system == 'Linux':
-            urlretrieve(
+            urllib.request.urlretrieve(
                 'https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.2.31/ncbi-blast-2.2.31+-x64-linux.tar.gz',
                 blast_file)
         elif platform_system == 'Windows':
-            urlretrieve(
+            urllib.request.urlretrieve(
                 'https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.2.31/ncbi-blast-2.2.31+-x64-win64.tar.gz',
                 blast_file)
         elif platform_system == 'Darwin':
-            urlretrieve(
+            urllib.request.urlretrieve(
                 'https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.2.31/ncbi-blast-2.2.31+-universal-macosx.tar.gz',
                 blast_file)
         else:
