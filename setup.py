6108983ae6d201ed5e9739eefc3dcb9ea95bd2e7
Christopher Childers <126100971+ChrisChilders-USDA@users.noreply.github.com>
2026-05-14 15:54:08 -0400

    Start low-risk pyproject rollout with packaging checks

--- setup.py
+++ setup.py
@@ -10,20 +10,59 @@ from distutils.command.build import build
 # To use a consistent encoding
 from codecs import open
 from os import path, remove, mkdir
+import platform
 import shutil
 import tarfile
 try:
-	from urllib.request import urlretrieve
+    from urllib.request import urlretrieve
 except ImportError:
-	from urllib import urlretrieve
+    from urllib import urlretrieve
 
-import platform
-import sys
-from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+try:
+    from setuptools.command.bdist_wheel import bdist_wheel as _bdist_wheel
+except ImportError:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 
 
 here = path.abspath(path.dirname(__file__))
 BLAST_VERSION = '2.17.0'
+
+
+def _blast_archive_name(version, platform_system):
+    if platform_system == 'Linux':
+        return 'ncbi-blast-{0:s}+-x64-linux.tar.gz'.format(version)
+    if platform_system == 'Windows':
+        return 'ncbi-blast-{0:s}+-x64-win64.tar.gz'.format(version)
+    if platform_system == 'Darwin':
+        return 'ncbi-blast-{0:s}+-universal-macosx.tar.gz'.format(version)
+    raise RuntimeError('GFF3 Toolkit currently only supports Linux, Windows, and MacOS')
+
+
+def bundle_blast(project_root, version):
+    platform_system = platform.system()
+    archive_name = _blast_archive_name(version, platform_system)
+
+    blast_path = path.join(project_root, 'gff3tool', 'lib', 'ncbi-blast+')
+    blast_file = path.join(blast_path, 'blast.tgz')
+    download_url = 'https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/{0:s}/{1:s}'.format(
+        version, archive_name)
+
+    if not path.exists(blast_path):
+        mkdir(blast_path)
+
+    urlretrieve(download_url, blast_file)
+
+    tar = tarfile.open(blast_file, 'r:gz')
+    tar.extractall(blast_path)
+    tar.close()
+
+    extract_path = path.join(blast_path, 'ncbi-blast-{0:s}+'.format(version))
+    shutil.move(path.join(extract_path, 'bin'), blast_path)
+
+    if path.exists(blast_file):
+        remove(blast_file)
+    if path.exists(extract_path):
+        shutil.rmtree(extract_path)
 
 
 class bdist_wheel(_bdist_wheel):
@@ -34,42 +73,8 @@ class bdist_wheel(_bdist_wheel):
 
 class CustomBuildCommand(build):
     def run(self):
-        platform_system = platform.system(
-        )  # Linux: Linux; Mac:Darwin; Windows: Windows
-
-        blast_path = path.join(here, 'gff3tool', 'lib', 'ncbi-blast+')
-        blast_file = path.join(blast_path, 'blast.tgz')
-
-        if not path.exists(blast_path):
-            mkdir(blast_path)
-
-        if platform_system == 'Linux':
-            urlretrieve(
-                'https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/{0:s}/ncbi-blast-{0:s}+-x64-linux.tar.gz'.format(BLAST_VERSION),
-                blast_file)
-        elif platform_system == 'Windows':
-            urlretrieve(
-                'https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/{0:s}/ncbi-blast-{0:s}+-x64-win64.tar.gz'.format(BLAST_VERSION),
-                blast_file)
-        elif platform_system == 'Darwin':
-            urlretrieve(
-                'https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/{0:s}/ncbi-blast-{0:s}+-universal-macosx.tar.gz'.format(BLAST_VERSION),
-                blast_file)
-        else:
-            sys.error(
-                'GFF3 Toolkit currently only supports linux, windows, and MacOS'
-            )
-
-        tar = tarfile.open(blast_file, 'r:gz')
-        tar.extractall(blast_path)
-        tar.close()
-
-        extract_path = path.join(blast_path, 'ncbi-blast-{0:s}+'.format(BLAST_VERSION))
-        shutil.move(path.join(extract_path, 'bin'), blast_path)
-        if path.exists(blast_file):
-            remove(blast_file)
-        if path.exists(extract_path):
-            shutil.rmtree(extract_path)
+        bundle_blast(here, BLAST_VERSION)
+        super(CustomBuildCommand, self).run()
 
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
